b0a81d3cbb5c7bde3331beeb42f73f6928ffaecf
kapadokia <ktnyandoch@gmail.com>
2022-11-03 18:23:22 +0300

    introduction

---
+++ a) counter-app/index.js
@@ -0,0 +1,16 @@
+// 1. ACCESSING DOM ELEMENT
+
+// in javascript, document is used to reffer the current page where this script will be called
+
+// below, we're getting the html element by id and set 5 as the new text
+document.getElementById("count").innerText = 5
+
+// everything after this double slash is called a line comment
+
+// 2. USING VARIABLES
+// We use let & const mostly, this replaces var
+let count = 12; 
+
+// we also use console.log(something) to print on cconsole the value or data we want to observe
+console.log(count)
+// to see this happening, go to any browser, prefferably chrome, then right click=>inspect=>console
